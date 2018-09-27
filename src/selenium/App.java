d3bf9c783548af9521b652f2e1bc871b9272fc5b
Leo Bogod <combatstriker111@gmail.com>
2018-09-26 19:03:22 -0500

    color of ping

--- src/selenium/App.java
+++ src/selenium/App.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package selenium;
-
+import java.awt.Color; 
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 import java.net.InetAddress;
 import java.util.GregorianCalendar;
 import java.util.*;
@@ -40,9 +44,39 @@ public  class App{
                 long lowest = Collections.min(list);
                   System.out.println("value" + lowest);
                sum = value+value/10;
-                jTextPane1.setText("Ping RTT: " + (value + "ms"));
-               jTextPane2.setText("average ping: " + sum + "ms");
+               if(value <250){
+                   
+                jTextPane1.setText("Ping RTT: " + value + "ms");
+                jTextPane2.setText("average ping: " + sum + "ms");
                jTextField1.setText("" + lowest + "ms");
+                    jTextPane1.setBackground(Color.green);
+                     jTextPane2.setForeground(Color.green);
+                       jTextField1.setBackground(Color.green);
+               }
+               
+               
+               else if(value>250 || value <500){
+                   
+                   jTextPane1.setText("Ping RTT: " + value + "ms");
+                jTextPane2.setText("average ping: " + sum + "ms");
+               jTextField1.setText("" + lowest + "ms");
+                    jTextPane1.setBackground(Color.yellow);
+                     jTextPane2.setBackground(Color.yellow);
+                       jTextField1.setBackground(Color.yellow);
+                   
+               }
+               
+               
+               else{
+                   
+                     jTextPane1.setText("Ping RTT: " + value + "ms");
+                jTextPane2.setText("average ping: " + sum + "ms");
+               jTextField1.setText("" + lowest + "ms");
+                    jTextPane1.setBackground(Color.red);
+                     jTextPane2.setBackground(Color.red);
+                       jTextField1.setBackground(Color.red);
+               }
+              
             } else {
                 System.out.println(ipAddress + " NOT reachable.");
             }
