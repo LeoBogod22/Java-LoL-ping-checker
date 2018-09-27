c925ff333549452b536630f7185e01f83922b574
Leo Bogod <combatstriker111@gmail.com>
2018-09-26 19:20:17 -0500

    Create constants.java

---
+++ src/main/constants.java
@@ -0,0 +1,19 @@
+package selenium;
+
+/**
+ *
+ * @author User
+ */
+public enum Constants {
+	NA("104.160.131.3"),EUW("104.160.141.3"),EUNE("104.160.142.3"),KOR("104.160.156.1"),LAN("104.160.136.3");
+	
+	private String ip;
+	
+	Constants(String ip){
+		this.ip = ip;
+	}
+	
+	public String getIp(){
+		return ip;
+	}
+}
